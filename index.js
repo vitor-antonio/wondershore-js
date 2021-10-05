b68fb2302c562e39225a24d898781c3fa3081444
Vitor Antonio <vitor.antonio@avanade.com>
2021-10-05 16:06:50 +0200

    added animation of pictures fade-in

--- index.js
+++ index.js
@@ -47,3 +47,45 @@ if (window.location.hash && currentHash === enHash) {
     element.textContent = altLanguages["en"][element.id];
   });
 }
+
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (element, dividend = 1) => {
+  const elementTop = element.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (element) => {
+  const elementTop = element.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
